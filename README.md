214d62e13d68dce16cc73c230375dffb0f1b8c78
Dominik Rost <dominik@fullflamingo.cc>
2023-12-04 11:12:41 +0100

    Add day 3 part 1

--- README.md
+++ README.md
@@ -11,9 +11,9 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
+![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -69,23 +69,23 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.32ms
-Time part 2: 1.82ms
-Both parts: 2.147ms
+Time part 1: -
+Time part 2: -
+Both parts: -
 ```
 
 ```
 Day 02
-Time part 1: 0.73ms
-Time part 2: 0.603ms
-Both parts: 1.333ms
+Time part 1: 0.647ms
+Time part 2: 0.682ms
+Both parts: 1.328ms
 ```
 
 ```
 Day 03
-Time part 1: -
+Time part 1: 7.209ms
 Time part 2: -
-Both parts: -
+Both parts: 7.209ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.333ms
+Total stars: 3/50
+Total time: 8.537ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.ts
@@ -0,0 +1,133 @@
+import run from "aocrunner";
+import { splitLines } from "../utils/index.js";
+
+const parseInput = (rawInput: string) => rawInput;
+
+interface Symbol {
+  symbol: string;
+  line: number;
+  index: number;
+}
+
+interface Number {
+  number: string;
+  line: number;
+  indexStart: number;
+  indexEnd: number;
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const lines = splitLines(input);
+
+  let sum = 0;
+
+  const symbols = getSymbols(lines);
+  const numbers = getNumbers(lines);
+
+  const adNums: Number[] = [];
+  symbols.forEach((symbol) => {
+    adNums.push(...getAdjecentNumbers(symbol, numbers));
+  });
+
+  adNums.forEach((adNum) => {
+    sum += parseInt(adNum.number, 10);
+  });
+  return sum;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+function getSymbols(lines: string[]) {
+  const symbols: Symbol[] = [];
+  lines.forEach((line, lineIndex) => {
+    const symMatches = line.matchAll(/[^a-zA-Z0-9\. ]/g);
+    for (let symMatch of symMatches) {
+      const sym: Symbol = {
+        index: symMatch.index,
+        line: lineIndex,
+        symbol: symMatch[0],
+      };
+      symbols.push(sym);
+    }
+  });
+  return symbols;
+}
+
+function getNumbers(lines: string[]) {
+  const numbers: Number[] = [];
+  lines.forEach((line, lineIndex) => {
+    const numMatches = line.matchAll(/[0-9]+/g);
+    for (const numMatch of numMatches) {
+      const num: Number = {
+        number: numMatch[0],
+        line: lineIndex,
+        indexStart: numMatch.index,
+        indexEnd: numMatch.index + numMatch[0].length - 1,
+      };
+      numbers.push(num);
+    }
+  });
+  return numbers;
+}
+
+function getAdjecentNumbers(symbol: Symbol, numbers: Number[]): Number[] {
+  const adNums: Number[] = [];
+  numbers.forEach((number) => {
+    if (
+      number.line === symbol.line ||
+      number.line === symbol.line - 1 ||
+      number.line === symbol.line + 1
+    ) {
+      if (
+        number.indexEnd === symbol.index - 1 ||
+        number.indexEnd === symbol.index ||
+        number.indexEnd === symbol.index + 1 ||
+        number.indexStart === symbol.index - 1 ||
+        number.indexStart === symbol.index ||
+        number.indexStart === symbol.index + 1
+      ) {
+        adNums.push(number);
+      }
+    }
+  });
+  return adNums;
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+        `,
+        expected: 4361,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
